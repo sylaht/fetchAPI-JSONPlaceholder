00241994de563bea860c58859419cf8450ff78bd
Thalys001 <thalyssouza708@gmail.com>
2023-01-21 14:37:11 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function fetchApiData() {
+  fetch("https://jsonplaceholder.typicode.com/todos/")
+    .then((response) => response.json())
+    .then((data) => {
+      const list = document.querySelector("#fill-list");
+
+      data.map((item) => {
+        const li = document.createElement("li");
+
+        li.setAttribute("id", item.id);
+        li.innerHTML = item.title;
+        list.appendChild(li);
+      });
+    });
+}
+// Consumindo API Json placeholder
